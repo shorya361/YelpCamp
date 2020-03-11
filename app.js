7495a480c29240b720bcbd4a3920411e5936381f
shorya361 <you@example.com>
2020-03-11 10:18:43 +0530

    connection to mongoATLAS

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var CampgroundRoutes=require("./routes/campgrounds"),
     commentRoutes   =require("./routes/comments"), 
     authRoutes      =require("./routes/index");
 
-mongoose.connect('mongodb+srv://punk_GTS:blackcaps434@shorya-moefa.mongodb.net/YelpCamp?retryWrites=true&w=majority', {useNewUrlParser: true , useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true , useUnifiedTopology: true }); // to connect to local database
+mongoose.connect('mongodb+srv://punk_GTS:blackcaps434@shorya-moefa.mongodb.net/YelpCamp_v1?retryWrites=true&w=majority', {useNewUrlParser: true , useUnifiedTopology: true }); //to connect to mongodb ATLAS database
 
 //seedDb(); seed the database
 
