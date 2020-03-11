13151a2be3693803625996314499b3f535a35048
shorya361 <you@example.com>
2020-03-11 12:43:30 +0530

    updating environmental variables

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ var Comment=require("./models/comment.js")
 var CampgroundRoutes=require("./routes/campgrounds"),
     commentRoutes   =require("./routes/comments"), 
     authRoutes      =require("./routes/index");
-
+var dbURL=process.env.DATABASEURL || "mongodb://localhost:27017/test"
 //mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true , useUnifiedTopology: true }); // to connect to local database
-mongoose.connect('mongodb+srv://punk_GTS:blackcaps434@shorya-moefa.mongodb.net/YelpCamp_v1?retryWrites=true&w=majority', {useNewUrlParser: true , useUnifiedTopology: true }); //to connect to mongodb ATLAS database
+mongoose.connect(dbURL, {useNewUrlParser: true , useUnifiedTopology: true }); //to connect to mongodb ATLAS database
 
 //seedDb(); seed the database
 
@@ -54,7 +54,10 @@ app.use((req,res,next)=>{
 app.use("",authRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 app.use("/campgrounds",CampgroundRoutes);
+
+// 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT,()=>
 {
     console.log("app started!!");  
